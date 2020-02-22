8e23d9371d0b522470e7c250dbe3b3abdd6a96e8
Cedric Mulvihill <cedric.mulvihill@gmail.com>
2020-02-21 20:20:27 -0500

    finishing up, need to adjust the reading/writing portion

--- nested.py
+++ nested.py
@@ -9,14 +9,76 @@ import sys
 
 
 def is_nested(line):
-    """Validate a single input line for correct nesting"""
-    pass
+    nest = line.split('')
+    i = 0
+    while i < len(nest):
+        if (nest[i] == '(') and (nest[i + 1] == '*'):
+            starSlice = nest[i + 1:]
+            if '*' in starSlice and starSlice[starSlice.index('*') + 1] == ')':
+                nest[i] = ''
+                nest[i+1] = ''
+                nest[i + 1 + starSlice.index('*')] = ''
+                nest[i + 2 + starSlice.index('*')] = ''
+        if nest[i] == '(':
+            parSlice = nest[i+1:]
+            if ')' in parSlice and (parSlice[parSlice.index(')') - 1] != '*'):
+                nest[i] = ''
+                nest[i + 1 + starSlice.index(')')] = ''
+        if nest[i] == '<':
+            anSlice = nest[i+1:]
+            if '>' in anSlice:
+                nest[i] = ''
+                nest[i+1 + anSlice.index('>')]
+        if nest[i] == '[':
+            sqSlice = nest[i+1:]
+            if ']' in sqSlice:
+                nest[i] = ''
+                nest[i + 1 + sqSlice.index(']')] = ''
+        if nest[i] == '{':
+            brSlice = nest[i+1:]
+            if '}' in brSlice:
+                nest[i] = ''
+                nest[i+1 + brSlice.index('}')] = ''
+    if '(' in nest:
+        return ('No', nest.index('('))
+    elif '[' in nest:
+        return ('No', nest.index('['))
+    elif '{' in nest:
+        return ('No', nest.index('{'))
+    elif '<' in nest:
+        return ('No', nest.index('<'))
+    elif '>' in nest:
+        return ('No', nest.index('>'))
+    elif '}' in nest:
+        return ('No', nest.index('}'))
+    elif ']' in nest:
+        return ('No', nest.index(']'))
+    elif ')' in nest:
+        return ('No', nest.index(')'))
+    else:
+        return 'Yes'
+
 
 
 def main(args):
-    """Open the input file and call `is_nested()` for each line"""
-    # Results: print to console and also write to output file
-    pass
+    c = 0
+    num = 0
+    lineList = []
+    with open (args, 'r') as f:
+        for line in f:
+            num += 1
+        while c < num:
+            lineList.append(is_nested(f.readLine()))
+            print(lineList[c])
+            c += 1
+    f.close()
+    w = open('output.txt', 'a')
+    for l in lineList:
+        w.write(l)
+    w.close()
+
+
+
 
 
 if __name__ == '__main__':
